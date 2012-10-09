e78b9b758a0a7ed2436f0013f9709ce530fa62d1
d.a.zanotti@gmail.com <d.a.zanotti@gmail.com@64492783-6dcb-8406-7555-21052510bab6>
2012-10-09 00:49:01 +0000

    Termine ajustarInventario y controlarVencimiento

--- trunk/src/negocio/Deposito.java
+++ trunk/src/negocio/Deposito.java
@@ -1,6 +1,7 @@
 package negocio;
 
 import java.util.Collection;
+import java.util.Date;
 
 public class Deposito {
 	protected Collection<ItemStock> stock;
@@ -26,5 +27,43 @@ public class Deposito {
 
 	public void setStock(Collection<ItemStock> stock) {
 		this.stock = stock;
-	}	
+	}
+	
+	public boolean ajustarInventario(Producto p, float cant){
+		if(buscarLoteMasViejo(p)!=null){
+			ItemStock it = buscarLoteMasViejo(p); 
+			if(it.getCantidad()>cant){
+				it.setCantidad(it.getCantidad()-cant);
+				return true;
+			}
+			return false;
+		}
+		else 
+			return false;
+	}
+	
+	private ItemStock buscarLoteMasViejo(Producto p){
+		ItemStock itViejo;
+		itViejo = null;
+		Date hoy = new Date();
+		for(ItemStock it : stock){
+			//Pregunto si el Lote no esta dado de Baja
+			if(it.getLote().getEstado()){
+				//Pregunto si este lote tiene este producto y si es mas viejo que el anterior
+				if(it.getLote().getProducto()==p && ((hoy.getTime()-it.getLote().getVencimiento().getTime())>(hoy.getTime()-itViejo.getLote().getVencimiento().getTime()))){
+					itViejo = it;
+				}
+			}
+		}
+		return itViejo;
+	}
+	
+	public void controlVencimientoLotes(){
+		Date hoy = new Date();
+		for(ItemStock it : stock){
+			if(it.getLote().getVencimiento().getTime()<hoy.getTime()){
+				it.getLote().setEstado(false);
+			}
+		}
+	}
 }
