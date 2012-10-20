42c2b85e6d1c89094f806c18153b10a62c64bd3c
emastronardi@gmail.com <emastronardi@gmail.com@64492783-6dcb-8406-7555-21052510bab6>
2012-10-20 18:12:17 +0000

    Agregado de Deposito a Area

--- trunk/src/negocio/Area.java
+++ trunk/src/negocio/Area.java
@@ -4,8 +4,22 @@ import java.util.Collection;
 
 public class Area {
 	protected Collection<Plato> platos;
-	protected Collection<ItemStock> stock;
+	//protected Collection<ItemStock> stock;
 	protected Collection<ListaReposicion> listasReposicion;
+	protected String nombre;
+	protected DepositoSucursal deposito;
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public DepositoSucursal getDeposito() {
+		return deposito;
+	}
+
+	public void setDeposito(DepositoSucursal deposito) {
+		this.deposito = deposito;
+	}
 
 	public Collection<Plato> getPlatos() {
 		return platos;
@@ -15,13 +29,13 @@ public class Area {
 		this.platos = platos;
 	}
 
-	public Collection<ItemStock> getStock() {
-		return stock;
-	}
-
-	public void setStock(Collection<ItemStock> stock) {
-		this.stock = stock;
-	}
+//	public Collection<ItemStock> getStock() {
+//		return stock;
+//	}
+//
+//	public void setStock(Collection<ItemStock> stock) {
+//		this.stock = stock;
+//	}
 
 	public Collection<ListaReposicion> getListasReposicion() {
 		return listasReposicion;
@@ -31,11 +45,18 @@ public class Area {
 		this.listasReposicion = listasReposicion;
 	}
 
-	public Area(Collection<Plato> platos, Collection<ItemStock> stock,
-			Collection<ListaReposicion> listasReposicion) {
+	public Area(Collection<Plato> platos, Collection<ItemStock> stock, Collection<ListaReposicion> listasReposicion, String nombre, DepositoSucursal depo) {
 		this.platos = platos;
-		this.stock = stock;
+//		this.stock = stock;
+		this.nombre = nombre;
 		this.listasReposicion = listasReposicion;
+		this.deposito = depo;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	
 	
 }
