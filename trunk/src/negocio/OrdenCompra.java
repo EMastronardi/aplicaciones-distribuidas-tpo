8cbcb83c1f42f5680c878dd3154210873a333a15
orellanoandres@gmail.com <orellanoandres@gmail.com@64492783-6dcb-8406-7555-21052510bab6>
2012-11-21 14:15:25 +0000

    Test, nueva máquina

--- trunk/src/negocio/OrdenCompra.java
+++ trunk/src/negocio/OrdenCompra.java
@@ -1,5 +1,14 @@
 package negocio;
 
 public class OrdenCompra {
+	private Proveedor proveedor;
+
+	public Proveedor getProveedor() {
+		return proveedor;
+	}
+
+	public void setProveedor(Proveedor proveedor) {
+		this.proveedor = proveedor;
+	}
 
 }
