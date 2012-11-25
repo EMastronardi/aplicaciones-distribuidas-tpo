c91eb989382c603e8aad9a1bba7c44732a1b0659
rdi <emastronardi@gmail.com>
2012-11-25 22:29:04 +0000

    mapeos de clase SEctor

--- src/negocio/Sector.java
+++ src/negocio/Sector.java
@@ -1,11 +1,24 @@
 package negocio;
 
 import java.util.Collection;
+
 import java.util.List;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Sectores")
 public class Sector {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int idSector;
 	private String nombre;
+	@OneToMany
+	@JoinColumn(name="idSector")
 	private List<Mesa> mesas;
+	
+	@OneToMany
+	@JoinColumn(name="idSector")
 	private List<Mozo> mozos;
 
 	public String getNombre() {
