f35441e5686a4b638c0f24dddca1908e4bc05635
d.a.zanotti@gmail.com <d.a.zanotti@gmail.com@64492783-6dcb-8406-7555-21052510bab6>
2012-12-02 18:21:25 +0000

    Carta

---
+++ trunk/src/negocio/Carta.java
@@ -0,0 +1,76 @@
+package negocio;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Cartas")
+public class Carta {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int idCarta;
+	private String nombre;
+	@OneToOne
+	@JoinColumn(name="idSuursal")
+	private Sucursal sucursal;
+	@OneToMany
+	@JoinColumn(name="idCarta")
+	private List<Rubro>  rubros;
+	
+	public Carta() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Carta(int idCarta, String nombre, Sucursal sucursal,
+			List<Rubro> rubros) {
+		super();
+		this.idCarta = idCarta;
+		this.nombre = nombre;
+		this.sucursal = sucursal;
+		this.rubros = rubros;
+	}
+
+	public int getIdCarta() {
+		return idCarta;
+	}
+
+	public void setIdCarta(int idCarta) {
+		this.idCarta = idCarta;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Sucursal getSucursal() {
+		return sucursal;
+	}
+
+	public void setSucursal(Sucursal sucursal) {
+		this.sucursal = sucursal;
+	}
+
+	public List<Rubro> getRubros() {
+		return rubros;
+	}
+
+	public void setRubros(List<Rubro> rubros) {
+		this.rubros = rubros;
+	}
+	
+	
+	
+}
