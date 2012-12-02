4638a8d0d655ca276b183fd7a5573041e963541f
d.a.zanotti@gmail.com <d.a.zanotti@gmail.com@64492783-6dcb-8406-7555-21052510bab6>
2012-12-02 18:25:00 +0000

    Rubro

---
+++ trunk/src/negocio/Rubro.java
@@ -0,0 +1,55 @@
+package negocio;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Rubros")
+public class Rubro {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int idRubro;
+	private String nombre;
+	@OneToMany
+	@JoinColumn(name="idPlato")
+	private List<Plato> platos;
+	
+	public Rubro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Rubro(int idRubro, String nombre, List<Plato> platos) {
+		super();
+		this.idRubro = idRubro;
+		this.nombre = nombre;
+		this.platos = platos;
+	}
+	
+	public int getIdRubro() {
+		return idRubro;
+	}
+	public void setIdRubro(int idRubro) {
+		this.idRubro = idRubro;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public List<Plato> getPlatos() {
+		return platos;
+	}
+	public void setPlatos(List<Plato> platos) {
+		this.platos = platos;
+	}
+	
+}
